8811fd0f6a545229f503133c9597730d1251ad76
ismail <coderismail22@gmail.com>
2024-08-22 09:29:48 +0600

    resolve type in baseAPI and change eslint config

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,8 +1,8 @@
 import { useGetAllSemestersQuery } from "../../../redux/features/academicSemester/academicSemesterApi";
 
 const AcademicSemester = () => {
-  const { data } = useGetAllSemestersQuery("");
-  console.log(data);
+  const { data } = useGetAllSemestersQuery(undefined);
+  console.log("all semester data", data?.data);
   return <div>AcademicSemester</div>;
 };
 
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,7 +1,13 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  DefinitionType,
+  FetchArgs,
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
-import { App } from "antd";
-import { setUser } from "../features/auth/authSlice";
+import { logout, setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -16,9 +22,14 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+//TODO: cookies neither setting automatically nor removing automatically
 //implementing custom baseQuery
-const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
-  const result = await baseQuery(args, api, extraOptions);
+const baseQueryWithRefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+  let result = await baseQuery(args, api, extraOptions);
   // console.log(result);
   if (result?.error?.status === 401) {
     console.log("Generating refresh token");
@@ -27,10 +38,16 @@ const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
       credentials: "include",
     });
     const data = await res.json();
-    const token = data.data.accessToken;
-    const user = (api.getState() as RootState).auth.user;
-    api.dispatch(setUser({ user, token }));
+    if (data?.data?.accessToken) {
+      const token = data.data.accessToken;
+      const user = (api.getState() as RootState).auth.user;
+      api.dispatch(setUser({ user, token }));
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
+    }
   }
+
   return result;
 };
 
