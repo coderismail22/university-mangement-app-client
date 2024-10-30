494d12db3593f954ab29a80b86ec7f6286d67b21
ismail <coderismail22@gmail.com>
2024-10-30 13:59:41 +0600

    make academicSemester creation form

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -2,21 +2,43 @@ import { Button, Col, Flex } from "antd";
 import NexusForm from "../../../components/form/NexusForm";
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import NexusSelect from "../../../components/form/NexusSelect";
+import { nameOptions } from "../../../constants/semester";
+import { monthOptions } from "../../../constants/global";
+
+const currentYear = new Date().getFullYear();
+const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
+  value: String(currentYear + number),
+  label: String(currentYear + number),
+}));
 
 const CreateAcademicSemester = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data);
+    const name = nameOptions[Number(data?.name) - 1]?.label;
+    const semesterData = {
+      name,
+      code: data?.name,
+      year: data?.year,
+      startMonth: data?.startMonth,
+      endMonth: data?.endMonth,
+    };
+    console.log(semesterData);
   };
-  const nameOptions = [
-    { value: "Autumn", label: "Autumn" },
-    { value: "Summer", label: "Summer" },
-    { value: "Fall", label: "Fall" },
-  ];
   return (
     <Flex justify="center" align="center">
       <Col span={8}>
         <NexusForm onSubmit={onSubmit}>
-          <NexusSelect label="Name" name="Name" options={nameOptions} />
+          <NexusSelect label="Name" name="name" options={nameOptions} />
+          <NexusSelect label="Year" name="year" options={yearOptions} />
+          <NexusSelect
+            label="Start Month"
+            name="startMonth"
+            options={monthOptions}
+          />
+          <NexusSelect
+            label="End Month"
+            name="endMonth"
+            options={monthOptions}
+          />
           <Button htmlType="submit">Submit</Button>
         </NexusForm>
       </Col>
